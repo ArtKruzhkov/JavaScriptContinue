2d43e1fa6be8153f228a8986490cc4c8917a00dc
ArtKruzhkov <112978976+ArtKruzhkov@users.noreply.github.com>
2024-02-11 18:45:16 +0300

    HW05

---
+++ HW5/data.js
@@ -0,0 +1,74 @@
+// Задание 1
+// 1. Поиск в интернете (бесплатные api примеры).
+// 2. Найти любые данные, на произвольную тему.
+// 3. Создать файл data.js.
+// 4. Создать переменную которая будет хранить данные из публичных api.
+const dataInfo = `[
+    {
+        "id": 1,
+        "name": "Rick Sanchez",
+        "status": "Alive",
+        "species": "Human",
+        "gender": "Male",
+        "origin": "Earth (C-137)",
+        "location": "Citadel of Ricks",
+        "image": "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+        "url": "https://rickandmortyapi.com/api/character/1"
+    },
+    {
+        "id": 2,
+        "name": "Morty Smith",
+        "status": "Alive",
+        "species": "Human",
+        "gender": "Male",
+        "origin": "unknown",
+        "location": "Citadel of Ricks",
+        "image": "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+        "url": "https://rickandmortyapi.com/api/character/2"
+    },
+    {
+        "id": 33,
+        "name": "Beebo",
+        "status": "Dead",
+        "species": "Alien",
+        "gender": "Male",
+        "origin": "Venzenulon 7",
+        "location": "Venzenulon 7",
+        "image": "https://rickandmortyapi.com/api/character/avatar/33.jpeg",
+        "url": "https://rickandmortyapi.com/api/character/33"
+    },
+    {
+        "id": 57,
+        "name": "Borpocian",
+        "status": "Alive",
+        "species": "Alien",
+        "gender": "Male",
+        "origin": "unknown",
+        "location": "unknown",
+        "image": "https://rickandmortyapi.com/api/character/avatar/57.jpeg",
+        "url": "https://rickandmortyapi.com/api/character/57"
+    },
+    {
+        "id": 99,
+        "name": "Hepatitis C",
+        "status": "Dead",
+        "species": "Disease",
+        "gender": "unknown",
+        "origin": "Anatomy Park",
+        "location": "Anatomy Park",
+        "image": "https://rickandmortyapi.com/api/character/avatar/99.jpeg",
+        "url": "https://rickandmortyapi.com/api/character/99"
+    },
+    {
+        "id": 132,
+        "name": "Gar's Mytholog",
+        "status": "Dead",
+        "species": "Mythological Creature",
+        "type": "Mytholog",
+        "gender": "Male",
+        "origin": "Nuptia 4",
+        "location": "Nuptia 4",
+        "image": "https://rickandmortyapi.com/api/character/avatar/132.jpeg",
+        "url": "https://rickandmortyapi.com/api/character/132"
+    }
+]`;
