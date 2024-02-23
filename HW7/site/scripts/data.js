e16fe98f9e965853488670f7ad636d9d703be2a6
ArtKruzhkov <112978976+ArtKruzhkov@users.noreply.github.com>
2024-02-23 17:53:49 +0300

    HW07

---
+++ HW7/site/scripts/data.js
@@ -0,0 +1,62 @@
+const dataProducts = `[
+    {
+        "id": "1",
+        "image": "C:/Users/artkr/OneDrive/Desktop/JavaScript(Continue)/Seminars/Seminar6/site/img/cartImg/cartImg1.jpg",
+        "alt": "cartImg1",
+        "name": "URBAN COMFORT HOODIE",
+        "price": "300$",
+        "color": "dark blue",
+        "size": "M",
+        "quantity": 1
+    },
+    {
+        "id": "2",
+        "image": "C:/Users/artkr/OneDrive/Desktop/JavaScript(Continue)/Seminars/Seminar6/site/img/cartImg/cartImg2.jpg",
+        "alt": "cartImg2",
+        "name": "Redlight Jacket",
+        "price": "278$",
+        "color": "black",
+        "size": "S",
+        "quantity": 1
+    },
+    {
+        "id": "2",
+        "image": "C:/Users/artkr/OneDrive/Desktop/JavaScript(Continue)/Seminars/Seminar6/site/img/cartImg/cartImg2.jpg",
+        "alt": "cartImg2",
+        "name": "Redlight Jacket",
+        "price": "278$",
+        "color": "blue",
+        "size": "XL",
+        "quantity": 1
+    },
+    {
+        "id": "2",
+        "image": "C:/Users/artkr/OneDrive/Desktop/JavaScript(Continue)/Seminars/Seminar6/site/img/cartImg/cartImg2.jpg",
+        "alt": "cartImg2",
+        "name": "Redlight Jacket",
+        "price": "278$",
+        "color": "yellow",
+        "size": "L",
+        "quantity": 1
+    },
+    {
+        "id": "2",
+        "image": "C:/Users/artkr/OneDrive/Desktop/JavaScript(Continue)/Seminars/Seminar6/site/img/cartImg/cartImg2.jpg",
+        "alt": "cartImg2",
+        "name": "Redlight Jacket",
+        "price": "278$",
+        "color": "light blue",
+        "size": "M",
+        "quantity": 1
+    },
+    {
+        "id": "2",
+        "image": "C:/Users/artkr/OneDrive/Desktop/JavaScript(Continue)/Seminars/Seminar6/site/img/cartImg/cartImg2.jpg",
+        "alt": "cartImg2",
+        "name": "Redlight Jacket",
+        "price": "278$",
+        "color": "olive",
+        "size": "S",
+        "quantity": 1
+    }
+]`;
---
+++ HW7/site/scripts/dataMain.js
@@ -0,0 +1,62 @@
+const dataProductsMain = `[
+    {
+        "id": 1,
+        "image": "C:/Users/artkr/OneDrive/Desktop/JavaScript(Continue)/HW/HW6/site/img/product1.jpg",
+        "alt": "productImg1",
+        "name": "RUGGED ELEGANCE MEN'S JACKET",
+        "link": "C:/Users/artkr/OneDrive/Desktop/JavaScript(Continue)/HW/HW6/site/catalogItem.html",
+        "desc": "Embrace the changing seasons with our men's autumn jacket. This rugged yet refined piece offers both durability and style for the cooler months ahead.",
+        "price": 52.00,
+        "currency": "$"
+    },
+    {
+        "id": 2,
+        "image": "C:/Users/artkr/OneDrive/Desktop/JavaScript(Continue)/HW/HW6/site/img/product2.png",
+        "alt": "productImg2",
+        "name": "BLACK HEELED SHOES",
+        "link": "C:/Users/artkr/OneDrive/Desktop/JavaScript(Continue)/HW/HW6/site/catalogItem.html",
+        "desc": "Discover the allure of our black heeled women's shoes—elegantly crafted to elevate every step with sophistication, style, and unparalleled comfort.",
+        "price": 71.00,
+        "currency": "$"
+    },
+    {
+        "id": 3,
+        "image": "C:/Users/artkr/OneDrive/Desktop/JavaScript(Continue)/HW/HW6/site/img/product3.png",
+        "alt": "productImg3",
+        "name": "URBAN COMFORT HOODIE",
+        "link": "C:/Users/artkr/OneDrive/Desktop/JavaScript(Continue)/HW/HW6/site/catalogItem.html",
+        "desc": "Explore our men's hoodies for unrivaled comfort and style. Crafted with premium materials, these versatile essentials are perfect for any occasion",
+        "price": 48.00,
+        "currency": "$"
+    },
+    {
+        "id": 4,
+        "image": "C:/Users/artkr/OneDrive/Desktop/JavaScript(Continue)/HW/HW6/site/img/product4.png",
+        "alt": "productImg4",
+        "name": "EFFORTLESS STYLE: MEN'S PANTS",
+        "link": "C:/Users/artkr/OneDrive/Desktop/JavaScript(Continue)/HW/HW6/site/catalogItem.html",
+        "desc": "Discover the epitome of comfort and style in our men's pants collection. From tailored classics to relaxed fits, find the perfect pair for every occasion.",
+        "price": 37.00,
+        "currency": "$"
+    },
+    {
+        "id": 5,
+        "image": "C:/Users/artkr/OneDrive/Desktop/JavaScript(Continue)/HW/HW6/site/img/product5.png",
+        "alt": "productImg5",
+        "name": "CHIC SOPHISTICATION: WOMEN'S BLAZER",
+        "link": "C:/Users/artkr/OneDrive/Desktop/JavaScript(Continue)/HW/HW6/site/catalogItem.html",
+        "desc": "Elevate your style with our women's blazer. Crafted for chic sophistication, this versatile piece adds polish and grace to any ensemble effortlessly.",
+        "price": 60.00,
+        "currency": "$"
+    },
+    {
+        "id": 6,
+        "image": "C:/Users/artkr/OneDrive/Desktop/JavaScript(Continue)/HW/HW6/site/img/product6.png",
+        "alt": "productImg6",
+        "name": "WOMEN'S ESSENTIAL TEE",
+        "link": "C:/Users/artkr/OneDrive/Desktop/JavaScript(Continue)/HW/HW6/site/catalogItem.html",
+        "desc": "Embrace casual sophistication with our women's essential tee. Crafted for comfort, its timeless design is perfect for effortless everyday style and versatility.",
+        "price": 36.00,
+        "currency": "$"
+    }
+]`;
